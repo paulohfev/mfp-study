6f671bed19a71f0a81a792977b8c27a5fc2727d9
Paulo Henrique Evangelista <phfernandesev@gmail.com>
2025-12-27 16:50:42 -0300

    Update container webpack prod and add public path

--- packages/container/config/webpack.prod.js
+++ packages/container/config/webpack.prod.js
@@ -9,6 +9,7 @@ const prodConfig = {
   mode: "production",
   output: {
     filename: "[name].[contenthash].js", // template for naming files when built
+    publicPath: "/container/latest/",
   },
   plugins: [
     new ModuleFederationPlugin({
