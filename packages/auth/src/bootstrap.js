d424d0d463b6ed0d887de9d33eb5d109c28971fc
Paulo Henrique Evangelista <phfernandesev@gmail.com>
2026-02-08 22:17:46 -0300

    Add auth mfe base setup

---
+++ packages/auth/src/bootstrap.js
@@ -0,0 +1,38 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import { createMemoryHistory, createBrowserHistory } from "history";
+import App from "./App";
+
+// Mount function to start up the app
+const mount = (el, { onNavigate, defaultHistory }) => {
+  const history = defaultHistory || createMemoryHistory();
+
+  if (onNavigate) {
+    history.listen(onNavigate);
+  }
+
+  ReactDOM.render(<App history={history} />, el);
+
+  return {
+    // any time the containers performs navigation, call this function
+    onParentNavigate({ pathname: nextPathName }) {
+      const { pathname } = history.location;
+
+      if (pathname !== nextPathName) {
+        history.push(nextPathName);
+      }
+    },
+  };
+};
+
+// If in development and in isolation, call mount immediately
+if (process.env.NODE_ENV === "development") {
+  const devRoot = document.querySelector("#_auth-dev-root");
+
+  if (devRoot) {
+    mount(devRoot, { defaultHistory: createBrowserHistory() });
+  }
+}
+
+// If running through container, export the mount function
+export { mount };
