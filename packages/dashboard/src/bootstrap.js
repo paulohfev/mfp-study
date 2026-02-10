8f1dfcc8e336cff354e2a83c7a6158cbcf4b3bc6
Paulo Henrique Evangelista <phfernandesev@gmail.com>
2026-02-09 23:03:42 -0300

    Add dashboard MFE base config

---
+++ packages/dashboard/src/bootstrap.js
@@ -0,0 +1,20 @@
+import { createApp } from "vue";
+import Dashboard from "./components/Dashboard.vue";
+
+// Mount function to start up the app
+const mount = (el) => {
+  const app = createApp(Dashboard);
+  app.mount(el);
+};
+
+// If in development and in isolation, call mount immediately
+if (process.env.NODE_ENV === "development") {
+  const devRoot = document.querySelector("#_dashboard-dev-root");
+
+  if (devRoot) {
+    mount(devRoot);
+  }
+}
+
+// If running through container, export the mount function
+export { mount };
